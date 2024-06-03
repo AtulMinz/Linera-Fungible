e62be985bd462f3cb86528b1bd9eef33bda84f14
Atulya Minz <atulminz.jsr@gamil.com>
2024-06-03 13:20:28 +0000

    Created instantiate function

--- src/contract.rs
+++ src/contract.rs
@@ -2,8 +2,10 @@
 
 mod state;
 
+use std::str::FromStr;
+
 use linera_sdk::{
-    base::WithContractAbi,
+    base::{Amount, WithContractAbi},
     views::{RootView, View, ViewStorageContext},
     Contract, ContractRuntime,
 };
@@ -11,18 +13,18 @@ use my_fungible::Message;
 
 use self::state::Fungible;
 
-pub struct ApplicationContract {
+pub struct FungibleContract {
     state: Fungible,
     runtime: ContractRuntime<Self>,
 }
 
-linera_sdk::contract!(ApplicationContract);
+linera_sdk::contract!(FungibleContract);
 
-impl WithContractAbi for ApplicationContract {
+impl WithContractAbi for FungibleContract {
     type Abi = my_fungible::ApplicationAbi;
 }
 
-impl Contract for ApplicationContract {
+impl Contract for FungibleContract {
     type Message = Message;
     type Parameters = ();
     type InstantiationArgument = ();
@@ -31,13 +33,21 @@ impl Contract for ApplicationContract {
         let state = Fungible::load(ViewStorageContext::from(runtime.key_value_store()))
             .await
             .expect("Failed to load state");
-        ApplicationContract { state, runtime }
+        FungibleContract { state, runtime }
     }
 
-    async fn instantiate(&mut self, _argument: Self::InstantiationArgument) {}
+    //The first time the application is created on a given chain.
+    async fn instantiate(&mut self, _argument: Self::InstantiationArgument) {
+        let amount = Amount::from_str("1_000_000").unwrap();
+        if let Some(owner) = self.runtime.authenticated_signer() {
+            self.state.initialize_accounts(owner, amount).await;
+        }
+    }
 
+    //Happens when a block is created with a transaction which contains an operation.
     async fn execute_operation(&mut self, _operation: Self::Operation) -> Self::Response {}
 
+    //How do we handle incoming message coming from a different chain.
     async fn execute_message(&mut self, _message: Self::Message) {}
 
     async fn store(mut self) {
