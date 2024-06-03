1f68e94a4a69bc15cb345172f8501c25ac872395
Atulya Minz <atulminz.jsr@gamil.com>
2024-06-03 12:19:14 +0000

    Implimentated Operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use linera_sdk::base::{ContractAbi, ServiceAbi};
+use linera_sdk::base::{Amount, ChainId, ContractAbi, Owner, ServiceAbi};
+use linera_sdk::graphql::GraphQLMutationRoot;
+use linera_sdk::views::linera_views::serde::{Deserialize, Serialize};
 
 pub struct ApplicationAbi;
 
@@ -10,4 +12,18 @@ impl ContractAbi for ApplicationAbi {
 impl ServiceAbi for ApplicationAbi {
     type Query = ();
     type QueryResponse = ();
+}
+
+#[derive(Debug, Deserialize, Serialize, GraphQLMutationRoot)]
+pub enum Operation {
+    Transfer {
+        owner: Owner,
+        amount: Amount,
+        target_account: Account,
+    },
+}
+
+pub struct Account {
+    pub chain_id: ChainId,
+    pub owner: Owner,
 }
