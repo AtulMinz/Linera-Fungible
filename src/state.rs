ce4f30f04a25105fd5214d698956c751c5261c2e
Atulya Minz <atulminz.jsr@gamil.com>
2024-06-03 12:58:05 +0000

    Added mutable funtions

--- src/state.rs
+++ src/state.rs
@@ -1,8 +1,37 @@
-use linera_sdk::views::{linera_views, RegisterView, RootView, ViewStorageContext};
+use linera_sdk::base::{Amount, Owner};
+use linera_sdk::views::{linera_views, MapView, RootView, ViewStorageContext};
 
 #[derive(RootView, async_graphql::SimpleObject)]
 #[view(context = "ViewStorageContext")]
-pub struct Application {
-    pub value: RegisterView<u64>,
-    // Add fields here.
+pub struct Fungible {
+    pub accounts: MapView<Owner, Amount>,
+}
+
+//Implement logic as wrapper aroung the state.
+
+//1. Initialise
+//2. Credit
+//3. Debit
+impl Fungible {
+    pub async fn initialize_accounts(&mut self, account: Owner, amount: Amount) {
+        self.accounts
+            .insert(&account, amount)
+            .expect("Error Initialising Accounts")
+    }
+
+    pub async fn balance(&self, account: &Owner) -> Amount {
+        self.accounts
+            .get(account)
+            .await
+            .expect("Failed to get balance")
+            .unwrap_or(Amount::ZERO)
+    }
+
+    pub async fn credit(&mut self, account: Owner, amount: Amount) {
+        let mut balance = self.balance(&account).await;
+        balance.saturating_add_assign(amount);
+        self.accounts
+            .insert(&account, balance)
+            .expect("Failed to credit")
+    }
 }
