d5daf2d3f31fea3dadb4108d5bf5eef95e76ccc1
Atulya Minz <atulminz.jsr@gmail.com>
2024-06-07 21:29:27 +0000

    Added Arc

--- src/service.rs
+++ src/service.rs
@@ -2,16 +2,22 @@
 
 mod state;
 
+use std::sync::{Arc, Mutex};
+
 use self::state::Fungible;
+use async_graphql::{EmptySubscription, Object, Schema};
 use linera_sdk::{
-    base::WithServiceAbi,
-    views::{View, ViewStorageContext},
+    base::{Amount, Owner, WithServiceAbi},
+    graphql::GraphQLMutationRoot,
+    views::{MapView, View, ViewStorageContext},
     Service, ServiceRuntime,
 };
+use my_fungible::Operation;
 
+#[derive(Clone)]
 pub struct FungibleService {
-    state: Fungible,
-    runtime: ServiceRuntime<Self>,
+    state: Arc<Fungible>,
+    runtime: Arc<Mutex<ServiceRuntime<Self>>>,
 }
 
 linera_sdk::service!(FungibleService);
@@ -28,11 +34,23 @@ impl Service for FungibleService {
         let state = Fungible::load(ViewStorageContext::from(runtime.key_value_store()))
             .await
             .expect("Failed to load state");
-        FungibleService { state, runtime }
+        FungibleService {
+            state: Arc::new(state),
+            runtime: Arc::new(Mutex::new(runtime)),
+        }
     }
 
     //Query coming from outside world how do we reason about giving back a response.
-    async fn handle_query(&self, _query: Self::Query) -> Self::QueryResponse {
-        panic!("Queries not supported by application");
+    async fn handle_query(&self, query: Self::Query) -> Self::QueryResponse {
+        let schema =
+            Schema::build(self.clone(), Operation::mutation_root(), EmptySubscription).finish();
+        schema.execute(query).await
+    }
+}
+
+#[Object]
+impl FungibleService {
+    async fn accounts(&self) -> &MapView<Owner, Amount> {
+        &self.state.accounts
     }
 }
